0dd92ff110c8450986372e9b26557e12ebec0f49
Felipe Oliveira <felipeoliveira264@gmail.com>
2026-08-26 14:43:31 -0300

    estrutura inicial do case

---
+++ src/test/java/com/credit/engine/srm/SrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.engine.srm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
