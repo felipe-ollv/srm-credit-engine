657a43b28cbb19baa359a7697e070b31bcc79e9d
Felipe Oliveira <felipeoliveira264@gmail.com>
2026-08-26 23:33:10 -0300

    configs para implemetacao/integracao do frontend

--- src/main/java/com/credit/engine/srm/config/OpenApiConfiguration.java
+++ src/main/java/com/credit/engine/srm/config/OpenApiConfiguration.java
@@ -3,6 +3,9 @@ package com.credit.engine.srm.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +19,14 @@ public class OpenApiConfiguration {
 						.title("SRM Credit Engine API")
 						.description("API para precificação e liquidação auditável de recebíveis")
 						.version("v1")
-						.contact(new Contact().name("SRM Credit Engine")));
+						.contact(new Contact().name("SRM Credit Engine")))
+				.components(new Components().addSecuritySchemes(
+						"bearerAuth",
+						new SecurityScheme()
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
 	}
 
 }
